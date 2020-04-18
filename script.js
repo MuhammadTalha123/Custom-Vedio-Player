5f10566ddbfb4fd7a04abbed9e27271c40f7debb
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-04-19 04:06:01 +0500

    commite 4

--- script.js
+++ script.js
@@ -1,51 +1,65 @@
-const video = document.getElementById('video')
-const play = document.getElementById('play')
-const stop = document.getElementById('stop')
-const progress = document.getElementById('progress')
-const timestamp = document.getElementById('timestamp')
+const video = document.getElementById("video");
+const play = document.getElementById("play");
+const stop = document.getElementById("stop");
+const progress = document.getElementById("progress");
+const timestamp = document.getElementById("timestamp");
 
 // play & pause video
 function toggleVideoStatus() {
-    if(video.paused){
-      video.play();
-    } else{
-        video.pause();
-    }
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
 }
 
 // update play/pause icons
 function updatePlayIcon() {
-    if(video.paused) {
-        play.innerHTML = '<i class="fa fa-play fa-2x"></i>'
-    } else{
-        play.innerHTML = '<i class="fa fa-pause fa-2x"></i>'
-    }
+  if (video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+  } else {
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+  }
 }
 
-// update progress
+// update progress timestamp
 function updateProgress() {
-    return true;
+  progress.value = (video.currentTime / video.duration) * 100;
+
+  //  get mins
+  let mins = Math.floor(video.currentTime / 60);
+  if (mins < 10) {
+    mins = "0" + String(mins);
+  }
+
+  // get secs
+  let secs = Math.floor(video.currentTime % 60);
+  if (secs < 10) {
+    secs = "0" + String(secs);
+  }
+  //     console.log(video.duration);
+  timestamp.innerHTML = `${mins}:${secs}`;
 }
 
 // set vidoe time to progress
 function setVideoProgress() {
-    return true;
+  video.currentTime = (+progress.value * video.duration) / 100;
 }
 
 // stop video
 function stopVideo() {
-    video.currentTime = 0;
-    video.pause(); 
+  video.currentTime = 0;
+  video.pause();
 }
 
 // Event listeners
-video.addEventListener('click', toggleVideoStatus);
-video.addEventListener('pause', updatePlayIcon);
-video.addEventListener('play', updatePlayIcon);
-video.addEventListener('progress', updateProgress);
+video.addEventListener("click", toggleVideoStatus);
+video.addEventListener("pause", updatePlayIcon);
+video.addEventListener("play", updatePlayIcon);
+video.addEventListener("timeupdate", updateProgress);
 
-play.addEventListener('click', toggleVideoStatus);
+play.addEventListener("click", toggleVideoStatus);
 
-stop.addEventListener('click', stopVideo);
+stop.addEventListener("click", stopVideo);
 
-progress.addEventListener('change', setVideoProgress);
+progress.addEventListener("change", setVideoProgress);
