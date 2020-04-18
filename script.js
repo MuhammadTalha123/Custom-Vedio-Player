0e5100401c7ac0790ce77625369d3980be457d11
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-04-19 02:28:43 +0500

    commite 2

---
+++ script.js
@@ -0,0 +1,51 @@
+const video = document.getElementById('video')
+const play = document.getElementById('play')
+const stop = document.getElementById('stop')
+const progress = document.getElementById('progress')
+const timestamp = document.getElementById('timestamp')
+
+// play & pause video
+function toggleVideoStatus() {
+    if(video.paused){
+      video.play();
+    } else{
+        video.pause();
+    }
+}
+
+// update play/pause icons
+function updatePlayIcon() {
+    if(video.paused) {
+        play.innerHTML = '<i class="fa fa-play fa-2x"></i>'
+    } else{
+        play.innerHTML = '<i class="fa fa-pause fa-2x"></i>'
+    }
+}
+
+// update progress
+function updateProgress() {
+    return true;
+}
+
+// set vidoe time to progress
+function setVideoProgress() {
+    return true;
+}
+
+// stop video
+function stopVideo() {
+    video.currentTime = 0;
+    video.pause(); 
+}
+
+// Event listeners
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('progress', updateProgress);
+
+play.addEventListener('click', toggleVideoStatus);
+
+stop.addEventListener('click', stopVideo);
+
+progress.addEventListener('change', setVideoProgress);
